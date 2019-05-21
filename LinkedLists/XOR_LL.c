6818233ed8be9e5c224e085f2d96b95a58ce3028
Kristopher Carroll <kjcarroll@alaska.edu>
2019-05-20 16:29:49 -0800

    added comments to better explain code

--- LinkedLists/XOR_LL.c
+++ LinkedLists/XOR_LL.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
+/* 
+ XOR_LL.c
+ Purpose: XOR Linked List implementation that allows for both forward and backward traversal similar to a doubly linked list
+          but only uses a single memory space for the XOR'ed value of the addresses of the previous Node and the next Node
+          this allows traversal in both directions without the need for two pointers
+*/
 
+
+
+// struct for int-holding Node
 struct Node {
     int data;
     struct Node* xor;
 };
-
+// XOR function to generate the XOR'ed addresses of previous node and next node
 struct Node* XOR (struct Node* prev, struct Node* next) {
     return (struct Node*) ((uintptr_t) (prev) ^ (uintptr_t) (next));
 }
-
+// creates a new Node from data and inserts in front of given Node* pointer
 void insert(struct Node** head, int data) {
     struct Node* new_node = (struct Node*) malloc (sizeof (struct Node));
     new_node->data = data;
@@ -22,7 +31,7 @@ void insert(struct Node** head, int data) {
     }
     *head = new_node;
 }
-
+// traverses the LL and prints data for each followed by a space
 void print_list (struct Node* head) {
     struct Node* curr = head;
     struct Node* prev = NULL;
@@ -30,7 +39,7 @@ void print_list (struct Node* head) {
 
     while (curr != NULL) {
         printf("%d ", curr->data);
-        next = XOR(prev, curr->xor);
+        next = XOR(prev, curr->xor); // need to calculate next by XOR'ing between the previous and our current XOR'ed value
         prev = curr;
         curr = next;
     }
